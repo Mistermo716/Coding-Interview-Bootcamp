79cee8f9af5dfd1b2eb7c82ea61fa47c158cb65a
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-29 01:50:58 -0700

    reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,4 @@
+'use strict';
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -6,6 +7,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//Bad Solution below
+// function reverse(str) {
+// 	//split by every character in the string and put into array.
+//   const arr = str.split('');
+// 	//reverses array
+//   arr.reverse();
+// 	//join the array indexes by every character.
+//   arr.join('');
+// }
 
+//Another Solution
+function reverse(str) {
+  let reversedStr = '';
+	//for of syntax
+  for (let char of str) {
+    reversedStr = char + reversedStr;
+  }
+  return reversedStr;
+}
+
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
 module.exports = reverse;
