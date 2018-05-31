7e92a387ba2ffdc8f947a551fa522fbc04927e98
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-30 19:26:59 -0700

    reverse int

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -16,4 +16,13 @@ function palindrome(str) {
   return str === reversed;
 }
 
+//Solution 2
+// function palindrome(str) {
+//   const reversed = str
+// 		.split('')
+// 		.reverse()
+// 		.join('');
+//   return str === reversed;
+// }
+
 module.exports = palindrome;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -1,3 +1,4 @@
+'use strict';
 // --- Directions
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
@@ -8,6 +9,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//Solution #1
+// function reverseInt(n) {
+//   let sign = Math.sign(n);
+//   let reversed = n
+// 		.toString()
+// 		.split('')
+// 		.reverse()
+// 		.join('');
+
+//   return parseInt(reversed) * sign;
+// }
+
+//Solution #2
+function reverseInt(n) {
+  let sign = Math.sign(n);
+  n = n.toString();
+  let reversedNum;
+  for (let digit of n) {
+    reversedNum = digit + reversedNum;
+  }
+  return parseInt(reversedNum) * sign;
+}
 
 module.exports = reverseInt;
