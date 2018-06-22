6a83858c2e7c40373ad4812fcdbeffc464fb4099
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-06-22 10:49:27 -0700

    chunked

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,30 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   let chunkedArr = [];
+
+//   for (let element of array) {
+//     const last = chunkedArr[chunkedArr.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunkedArr.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunkedArr;
+// }
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
